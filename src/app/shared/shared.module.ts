e192f4a0b0d00b0f88b3db3fe5f76637f7a99dd2
Ananda Widiprabawa <ananda.widiprabawa@gmail.com>
2021-12-05 20:25:16 +0800

    feat: add navigation menu

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BoardModule } from './components/board/board.module';
 import { CenteredLayoutModule } from './components/centered-layout/centered-layout.module';
+import { NavMenuModule } from './components/nav-menu/nav-menu.module';
 import { PrimaryButtonModule } from './components/primary-button/primary-button.module';
 import { WinnerDialogModule } from './components/winner-dialog/winner-dialog.module';
 
@@ -12,6 +13,7 @@ import { WinnerDialogModule } from './components/winner-dialog/winner-dialog.mod
     PrimaryButtonModule,
     BoardModule,
     WinnerDialogModule,
+    NavMenuModule,
   ],
 })
 export class SharedModule {}
