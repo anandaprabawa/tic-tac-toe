cc7e29e72710c5b83268ee094feddae34c0a9ea7
Ananda Widiprabawa <ananda.widiprabawa@gmail.com>
2021-12-05 20:06:23 +0800

    feat: add logo

--- src/app/main-menu/main-menu.module.ts
+++ src/app/main-menu/main-menu.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { SharedModule } from '../shared/shared.module';
 import { MainMenuRoutingModule } from './main-menu-routing.module';
 import { MainMenuComponent } from './pages/main-menu/main-menu.component';
 
 @NgModule({
   declarations: [MainMenuComponent],
-  imports: [CommonModule, MainMenuRoutingModule, SharedModule],
+  imports: [CommonModule, MainMenuRoutingModule, SharedModule, MatIconModule],
 })
 export class MainMenuModule {}
